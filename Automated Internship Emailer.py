d15799b522aeb36d1a34181368b5e67992339a6b
Aditya Sen <adityasen120@gmail.com>
2023-02-03 14:00:40 -0500

    round 4 of cold emailing

--- Automated Internship Emailer.py
+++ Automated Internship Emailer.py
@@ -18,7 +18,7 @@ PASSWORD = os.getenv("PASSWORD")
 #can change content of cover letter and email body + subject
 def send_email_companies(subject_email_address, subject_name) -> None:
     #change subject each specific company TODO Change subject name
-    subject = f"Summer Internship - {subject_name}"
+    subject = f"Experienced, Inexpensive, and Looking: Part-Time Job {subject_name}"
     #INFO OF EMAIL
     #set where credentials of email, TO, FROM, SUBJECT
     message = EmailMessage()
@@ -33,23 +33,34 @@ def send_email_companies(subject_email_address, subject_name) -> None:
     f"""\
 Hello {subject_name},
 
-I'm Aditya, a Grade 11 student in GTA Toronto. 
+I hope you are doing well.
 
-For the summer I am looking for a tech internship. As I was searching through the internet for tech companies around Canada, {subject_name} caught my eye.
+Hi, I'm Aditya Sen. A passionate developer looking for a challenge, in grade 12.
+Looking through top tech startups in Toronto, {subject_name} caught my eye. 
 
-I was wondering if I am able to apply for an internship to your company as a developer. I've attached my resume to this email if you want to look into that. 
+I'm currently looking for a part-time development job opportunity, and {subject_name} one of the companies that I believe can satisfy my goal.
 
-I also have a personal blog website, that I made with the MERN tech stack, if you want to learn more about me :)
-I'm always looking for opportunities to learn more about this industry, be it web-development or any other.
+I understand that companies are always on the lookout for talent. 
+I understand that most startups are in a fast-paced pressured environment. 
+I understand that startups are also bootstrapped for funds.
+
+Unfortunately, since I am in Grade 12, I do have time constraints due to my school hours. 
+But with that if you are looking for cost-effective labour, and a determined worker. I'm looking for a company to challenge me, and to learn from. Learning about the tech industry and startup/company culture.
+
+If my resume, Github, experience, or projects don't prove to you that I have the necessary skills. Feel free to send me an assignment, or test on your tech stack. Or to set up an interview. I would be happy to satisfy your needs.
 
 LinkedIN: https://www.linkedin.com/in/aditya-s3n/
 Github: https://github.com/aditya-s3n
 Personal Blog Website: https://sol-thoughts.web.app
 
 I'm looking forward to hearing from you!
+
 ---
+Best Regards,
 {NAME}
 {EMAIL}
+
+Sent through personal python email script: https://github.com/aditya-s3n/Email-Automation
     """)
 
     #ATTACHING TO EMAIL
@@ -57,7 +68,7 @@ I'm looking forward to hearing from you!
     #resume
     with open('AdityaSen_Resume.pdf', 'rb') as content_file:
         content = content_file.read()
-        message.add_attachment(content, maintype='application', subtype='pdf', filename='Aditya Sen Resume - High School.pdf')
+        message.add_attachment(content, maintype='application', subtype='pdf', filename='AdityaSen_Resume.pdf')
 
 
     #send the email
@@ -77,7 +88,7 @@ I'm looking forward to hearing from you!
 email_check = []
 #get company name and emails
 #iterate through all the rows in the csv file
-job_csv = open("Round 3 - Sheet1.csv")
+job_csv = open("Email_List.csv")
 job_csv.readline()
 job_csv = csv.reader(job_csv)
 #change company email to each specfic company
@@ -105,4 +116,5 @@ for data in job_csv:
 
 #prints out how many emails were successfully sent
 for email in email_check:
+    #print(email);
     print(f'{email[0]:<40}: {email[1]:>30}')
