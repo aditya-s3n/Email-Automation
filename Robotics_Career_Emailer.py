2d53e2222499d41bd55e7e5a039be346f3a2e5ad
Aditya Sen <adityasen120@gmail.com>
2023-02-13 13:55:50 -0500

    Add Robotics Emailer for Seek Professionals

--- Robotics_Career_Emailer.py
+++ Robotics_Career_Emailer.py
@@ -35,40 +35,46 @@ Hello {subject_name},
 
 I hope you are doing well.
 
-Hi, I'm Aditya Sen. A passionate developer looking for a challenge, in grade 12.
-Looking through top tech startups in Toronto, {subject_name} caught my eye. 
+Hi, I'm Aditya Sen. A grade 12 student in Canada. For our Grade 12 Robotics class, we have an assignment to research potential career paths in Robotics, and interview an industry professional.
+The career path a choose to research is Electrical Engineer.
+If it is not too much of a hassle, may you, or someone in your company (preferably an electrical engineer or a similar discipline), answer 12 questions for my assignment.
+You, or they, may answer the questions to any degree they see fit.
 
-I'm currently looking for a part-time development job opportunity, and {subject_name} one of the companies that I believe can satisfy my goal.
+The Questions: 
 
-I understand that companies are always on the lookout for talent. 
-I understand that most startups are in a fast-paced pressured environment. 
-I understand that startups are also bootstrapped for funds.
+1. What is your job title? 
+2. What are the duties and responsibilities of your job? 
+3. How many hours per day or week do you work? Do you work shifts? 
+4. Can you tell me about your background and how you got into this field? 
+5. 	a) What do you like the most about your work? 
+    b) What do you like the least about your work?
+6 	a) What education or training is needed for this occupation? 
+    b) What training have you completed since starting the job? 
+    c) What personal characteristics are required for someone to be successful in this job? 
+7. Is there a steady demand for workers in this field? How much job security is there? 
+8. What should people do to get started in this career? (i.e. experience, training, education) 
+9. How might this job change in the future? 
+10. What other jobs could you do with the skills/education you have gained in this field. 
+11. How are new employees hired for this position? 
+12. What supplementary skills/knowledge, that is not engineering-related, are handy for this job? (e.g interpersonal skills, accounting, knowledge of equity and shares)
 
-Unfortunately, since I am in Grade 12, I do have time constraints due to my school hours. 
-But with that if you are looking for cost-effective labour, and a determined worker. I'm looking for a company to challenge me, and to learn from. Learning about the tech industry and startup/company culture.
+Again, feel free answer the questions to however much specificity you deem comfortable. One-sentence answers are fine. 
 
-If my resume, Github, experience, or projects don't prove to you that I have the necessary skills. Feel free to send me an assignment, or test on your tech stack. Or to set up an interview. I would be happy to satisfy your needs.
-
-LinkedIN: https://www.linkedin.com/in/aditya-s3n/
-Github: https://github.com/aditya-s3n
-Personal Blog Website: https://sol-thoughts.web.app
-
-I'm looking forward to hearing from you!
+Thank you so much!
+Looking forward to hearing from you!
 
 ---
 Best Regards,
 {NAME}
 {EMAIL}
-
-Sent through personal python email script: https://github.com/aditya-s3n/Email-Automation
     """)
 
     #ATTACHING TO EMAIL
     #attach resume & cover letter to email
     #resume
-    with open('AdityaSen_Resume.pdf', 'rb') as content_file:
-        content = content_file.read()
-        message.add_attachment(content, maintype='application', subtype='pdf', filename='AdityaSen_Resume.pdf')
+    # with open('AdityaSen_Resume.pdf', 'rb') as content_file:
+    #     content = content_file.read()
+    #     message.add_attachment(content, maintype='application', subtype='pdf', filename='AdityaSen_Resume.pdf')
 
 
     #send the email
@@ -88,7 +94,7 @@ Sent through personal python email script: https://github.com/aditya-s3n/Email-A
 email_check = []
 #get company name and emails
 #iterate through all the rows in the csv file
-job_csv = open("Email_List.csv")
+job_csv = open("")
 job_csv.readline()
 job_csv = csv.reader(job_csv)
 #change company email to each specfic company
@@ -96,8 +102,8 @@ for data in job_csv:
     subject_name_global = data[0] #company name
 
     if data[3] != '': #company email
-        #company_email = data[3]
-        company_email = "1senadi@hdsb.ca"
+        company_email = data[3]
+        # company_email = "1senadi@hdsb.ca"
 
         #send email & check if it went successfully through
         temp_list = []
